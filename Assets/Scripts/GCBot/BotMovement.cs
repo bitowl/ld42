d53d129d5e8cd08a825f91d4deba0f9b17aed4ec
bitowl <github@bitowl.de>
2018-08-11 10:15:12 +0200

    Add super basic bot control

---
+++ Assets/Scripts/GCBot/BotMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotMovement : MonoBehaviour {
+	public float MoveSpeed = 10;
+	public float RotationSpeed = 10;
+
+	private Rigidbody rb;
+
+	private float horizontalInput;
+	private float verticalInput;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontalInput = Input.GetAxis("Horizontal");
+		verticalInput = Input.GetAxis("Vertical");
+	}
+
+	void FixedUpdate()
+	{
+		rb.AddForce(transform.forward * verticalInput * MoveSpeed - rb.velocity, ForceMode.Force);
+		rb.AddTorque(transform.up * RotationSpeed * horizontalInput - rb.angularVelocity);
+	}
+}
